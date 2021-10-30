cc6585488aea738329dcd59b55d74117e30025ab
Yiming Wang <89873516+1178594022@users.noreply.github.com>
2021-10-30 02:56:54 -0400

    Update assignment_2.py

--- Assignment_2_code/assignment_2.py
+++ Assignment_2_code/assignment_2.py
@@ -1,5 +1,17 @@
 from mediawiki import MediaWiki
+import urllib.request
+import random
+import string
+import itertools 
 wikipedia = MediaWiki()
 babson = wikipedia.page("Babson College")
 print(babson.title)
-print(babson.content)
+# print(babson.content)
+
+data = wikipedia.page("Babson College")
+
+for line in data:
+    if line.startwith('==='):
+        continue
+    else:
+        data.line = line
