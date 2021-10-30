2579f76623ffd78ac0c463e4106619d6cb2e4098
Yiming Wang <89873516+1178594022@users.noreply.github.com>
2021-10-30 17:17:13 -0400

    1

--- Assignment_2_code/assignment_2.py
+++ Assignment_2_code/assignment_2.py
@@ -1,17 +1,38 @@
+from os import write
 from mediawiki import MediaWiki
 import urllib.request
 import random
 import string
-import itertools 
+import itertools
+import re
+
+import mediawiki 
 wikipedia = MediaWiki()
-babson = wikipedia.page("Babson College")
-print(babson.title)
-# print(babson.content)
+MJ = wikipedia.page("Michael Jackson")
+strippables = string.punctuation + string.whitespace
+subed_MJ = re.sub(strippables,' ',MJ.content)
+print(subed_MJ)
 
-data = wikipedia.page("Babson College")
+# data = babson.content
+# with open('babson.txt', 'w') as f:
+#     for line in data:
+#         if line.startswith('='):
+#             continue
+#         else:
+#             f.write(line)
 
-for line in data:
-    if line.startwith('==='):
-        continue
-    else:
-        data.line = line
+# data1 = open(babson.txt)
+
+# strippables = string.punctuation + string.whitespace
+
+# for line in data1:
+#     print(line)
+# l = []
+
+# for line in data:
+#     if line.startswith('==='):
+#         continue
+#     else:
+#         l.append(line)
+
+# print (l)
--- Assignment_2_code/code.py
+++ Assignment_2_code/code.py
@@ -2,48 +2,74 @@ import urllib.request
 import random
 import string
 import itertools 
+import rlcompleter
+import re
+import operator
 
 url = 'https://www.gutenberg.org/cache/epub/66627/pg66627.txt'
 response = urllib.request.urlopen(url)
 data = response.read()
 text = data.decode('utf-8')
+nltk_text = data.decode('utf-8')
 
-def process_file(text, skip_header):
-    """Makes a histogram that contains the words from a file.
-    filename: string
-    skip_header: boolean, whether to skip the Gutenberg header
-    returns: map from each word to the number of times it appears.
-    """
-    hist = {}
-    text
+strippables = r"(?:s|'s|!+|,|\.|;|:|\(|\)|\"|\?+)?\s"
+text = re.sub(strippables, ' ',text)
+text = text.lower()
 
-    if skip_header:
-        skip_gutenberg_header(text)
+def count_words(book):
+    word_count = {}
+    for i in book.split():
+        word_count[i] = word_count.get(i,0)+1
+    return word_count
 
-    strippables = string.punctuation + string.whitespace
 
-    for line in text:
-        if line.startswith('THE END'):
-            break
+def ranked_words(dic):
+    sorted_values = sorted(dic.values(),reverse=True) # Sort the values
+    sorted_dict = {}
 
-        line = line.replace('-', ' ')
+    for i in sorted_values:
+        for k in dic.keys():
+            if dic[k] == i:
+                sorted_dict[k] = dic[k]
 
-        for word in line:
-            # word could be 'Sussex.'
-            word = word.strip(strippables)
-            word = word.lower()
+    return(sorted_dict)
 
-            # update the dictionary
-            hist[word] = hist.get(word, 0) + 1
 
-    return hist
+def top_x_words(dic,x):
+    sorted_values = sorted(dic.values(),reverse=True) # Sort the values
+    sorted_dict = {}
+    sorted_top = []
+    for i in range(10):
+        sorted_top.append(sorted_values[i])
 
-def skip_gutenberg_header(fp):
-    """Reads from fp until it finds the line that ends the header.
-    fp: open file object
-    """
-    for line in fp:
-        if line.startswith('*** START OF THE PROJECT GUTENBERG EBOOK THE CAT\'S PAW ***'):
-            break
+    for i in sorted_top:
+        for k in dic.keys():
+            if dic[k] == i:
+                sorted_dict[k] = dic[k]
 
-print(process_file(text, skip_header = True ))
+    return(sorted_dict)
+
+def compare_top_x(dict_1,dict_2,x):
+    d1 = top_x_words(dict_1,x)
+    d2 = top_x_words(dict_2,x)
+    l = []
+    for key in d1:
+        if key not in d2.keys():
+            l.append(key)
+    for key in d2:
+        if key not in d1.keys():
+            l.append(key)
+    no_dup_list = []
+    for i in l:
+        if i not in no_dup_list:
+            no_dup_list.append(i)
+    return no_dup_list
+
+
+import nltk
+nltk.download('vader_lexicon')
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
+sentence = nltk_text
+score = SentimentIntensityAnalyzer().polarity_scores(sentence)
+print(score)
